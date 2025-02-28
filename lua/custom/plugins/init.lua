a5d739147b343f742b33cababea82adcdf2dd3c8
Tripurari Shankar Singh <tripurari.s@uber.com>
2025-02-28 12:22:27 +0530

    use fzf-lua for file search (performance in monorepos)

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'ibhagwan/fzf-lua',
+    -- optional for icon support
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    config = function()
+      require('fzf-lua').setup {}
+      local fzf = require 'fzf-lua'
+      vim.keymap.set('n', '<leader>sf', function()
+        fzf.files()
+      end, { desc = '[S]earch [F]iles' })
+    end,
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
