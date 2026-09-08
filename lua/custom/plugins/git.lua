1b834e6fa0089c31722afb2ef0808de6cc0d712e
Tripurari Shankar Singh <tripurari.shankar.singh@proton.ch>
2026-09-08 16:51:09 +0530

    updated the diff plugin

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -2,14 +2,14 @@
 vim.pack.add { 'https://github.com/tpope/vim-fugitive' }
 
 
--- Diff hilighting
-vim.g.diffs = {
-  view = {
-    default_layout = 'split',
-  }
-}
-
-vim.pack.add({
-  'https://github.com/barrettruth/diffs.nvim',
+vim.pack.add({ "https://github.com/CoreyKaylor/diffbandit.nvim" })
+require("diffbandit").setup({
+  navigation = {
+    align_on_jump = true,
+  },
+  ui = {
+    theme = {
+      min_background_delta = 0.5 -- default: 0.08
+    },
+  },
 })
-
